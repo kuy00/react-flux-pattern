7c084f5d63410e014affda72a666849ad3f4245e
kuy <kuy472013@gmail.com>
2023-02-05 23:17:41 +0900

    state 저장

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,13 @@
 import { createStore } from 'redux';
 import rootReducer from './modules';
 
-const store = createStore(rootReducer);
+// Check Localstorage
+const persistedState = localStorage.getItem('reduxState')
+  ? JSON.parse(localStorage.getItem('reduxState'))
+  : {};
+
+const store = createStore(rootReducer, persistedState);
+store.subscribe(() => {
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+});  
 export default store;
